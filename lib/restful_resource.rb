b74db23611e6c05d93752d08df34d898986c4f15
Akash B <akash@carwow.co.uk>
2015-12-09 09:43:27 +0000

    added resolv-replace in attempt to fix socketerrors

--- lib/restful_resource.rb
+++ lib/restful_resource.rb
@@ -4,16 +4,18 @@ require 'link_header'
 require 'restclient'
 require 'active_support'
 require 'active_support/all'
-require_relative "restful_resource/version"
+require 'resolv-replace.rb'
+require_relative 'restful_resource/version'
 require_relative 'restful_resource/null_logger'
-require_relative "restful_resource/paginated_array"
-require_relative "restful_resource/parameter_missing_error"
-require_relative "restful_resource/resource_id_missing_error"
-require_relative "restful_resource/open_object"
+require_relative 'restful_resource/paginated_array'
+require_relative 'restful_resource/parameter_missing_error'
+require_relative 'restful_resource/resource_id_missing_error'
+require_relative 'restful_resource/open_object'
 require_relative 'restful_resource/response'
 require_relative 'restful_resource/authorization'
 require_relative 'restful_resource/http_client'
-require_relative "restful_resource/associations"
+require_relative 'restful_resource/associations'
 require_relative 'restful_resource/rails_validations'
-require_relative "restful_resource/base"
-require_relative "restful_resource/old_base"
+require_relative 'restful_resource/base'
+require_relative 'restful_resource/old_base'
+

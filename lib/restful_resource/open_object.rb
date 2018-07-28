5f2bf756abc2bbf28fbfde7b4b4954e515b84b23
Ken Alex Fassone <nexusventuri@gmail.com>
2018-07-28 14:58:09 +0100

    Optimize open object access
    Based on this study: https://github.com/nexusventuri/ruby-json-parsing

--- lib/restful_resource/open_object.rb
+++ lib/restful_resource/open_object.rb
@@ -1,22 +1,57 @@
 module RestfulResource
   class OpenObject
-    def initialize(attributes = {}, hack_for_activeresource = false)
-      @inner_object = OpenStruct.new(attributes)
+    def initialize(attributes = {}, _hack_for_activeresource = false)
+      @inner_object = {}
+      attributes.each { |k, v| @inner_object[k.to_sym] = v }
     end
 
-    def method_missing(method, *args, &block)
-      if @inner_object.respond_to?(method)
-        @inner_object.send(method, *args, &block)
+    def new_enhanced_open_object_member!(name) # :nodoc:
+      name = name.to_sym
+      unless singleton_class.method_defined?(name)
+        define_singleton_method(name) { @inner_object[name] }
+        define_singleton_method("#{name}=") { |x| name = x }
+      end
+      name
+    end
+
+    def []=(key, val)
+      @inner_object[key] = val
+    end
+
+    def [](key)
+      @inner_object[key]
+    end
+
+    private :new_enhanced_open_object_member!
+
+    def method_missing(mid, *args) # :nodoc:
+      len = args.length
+      if mid[/.*(?==\z)/m]
+        if len != 1
+          raise ArgumentError, "wrong number of arguments (#{len} for 1)", caller(1)
+        end
+      elsif len.zero?
+        if @inner_object.key?(mid)
+          new_enhanced_open_object_member!(mid) unless frozen?
+          @inner_object[mid]
+        else
+          raise NoMethodError
+        end
       else
-        super(method)
+        begin
+          super
+        rescue NoMethodError => err
+          err.backtrace.shift
+          raise
+        end
       end
     end
 
     def respond_to?(method, include_private = false)
-      super || @inner_object.respond_to?(method, include_private)
+      super || @inner_object.key?(method)
     end
 
-    def as_json(options=nil)
+    def as_json(options = nil)
       @inner_object.send(:table).as_json(options)
     end
 

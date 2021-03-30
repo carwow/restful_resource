b8da65b442ccab2371d7dee72fa18f55197a5f0e
tom-lord <lord.thom@gmail.com>
2021-03-30 11:28:48 +0100

    Add custom deprecation message

--- lib/restful_resource/strict_open_struct.rb
+++ lib/restful_resource/strict_open_struct.rb
@@ -1,5 +1,5 @@
 module RestfulResource
   class StrictOpenStruct < ::OpenStruct
-    deprecate :dig, :[]
+    deprecate :dig, :[], deprecator: ActiveSupport::Deprecation.new('3.0.0', 'restful_resource')
   end
 end

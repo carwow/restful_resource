3811e03822c8efe07ec9d9c2b14c1c1ab0cd37f8
tom-lord <lord.thom@gmail.com>
2021-05-14 14:17:09 +0100

    Make it actually recursive

--- lib/restful_resource/open_object.rb
+++ lib/restful_resource/open_object.rb
@@ -1,9 +1,7 @@
 module RestfulResource
   class OpenObject
     def initialize(attributes = {}, _hack_for_activeresource = false)
-      @inner_object = StrictOpenStruct.new(
-        attributes.transform_values { |v| v.is_a?(Hash) ? StrictOpenStruct.new(v) : v }
-      )
+      @inner_object = StrictOpenStruct.recursively_build(attributes)
     end
 
     def method_missing(method, *args, &block)
--- lib/restful_resource/strict_open_struct.rb
+++ lib/restful_resource/strict_open_struct.rb
@@ -1,5 +1,11 @@
 module RestfulResource
   class StrictOpenStruct < ::OpenStruct
+    def self.recursively_build(attributes)
+      new(attributes.transform_values do |attr|
+        attr.is_a?(Hash) ? recursively_build(attr) : attr
+      end)
+    end
+
     deprecate :dig, :[]
   end
 end

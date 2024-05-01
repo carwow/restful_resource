7c0c7e89e222fa6e8f5f4af727a6c7145f04f392
tom-lord <lord.thom@gmail.com>
2024-05-01 15:38:22 +0100

    v2.18.1 -- Explicitly require ostruct

--- lib/restful_resource/strict_open_struct.rb
+++ lib/restful_resource/strict_open_struct.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 module RestfulResource
   class StrictOpenStruct < ::OpenStruct
     deprecate :dig, :[], deprecator: Base::Deprecator
--- lib/restful_resource/version.rb
+++ lib/restful_resource/version.rb
@@ -1,3 +1,3 @@
 module RestfulResource
-  VERSION = '2.18.0'.freeze
+  VERSION = '2.18.1'.freeze
 end
